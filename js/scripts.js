e5a931431b0192835e2d3e327a65bb6f6366857f
Zarina Mazieva <zmazieva78@gmail.com>
2019-10-29 19:20:41 -0700

    add forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,21 @@ $(function() {
 
   $("#submitButton").click(function() {
     var userInput = $("input.form-control").val();
-    var word = userInput.split(" ");
-    console.log(word);
-    // var char = userInput.split("");
-    // console.log(char[8]);
-    // var inputArrayCopy = userInput.split();
-    // console.log(inputArrayCopy);
-    // // var char = inputArray.
+    var words = userInput.split(" ");
+    console.log(words[2]);
+    var chars = userInput.split("");
+    console.log(chars[8]);
+    var inputArrayCopy = userInput.split();
+    console.log(inputArrayCopy);
+    var reversedWords = words.reverse();
 
+    var str = "";
 
-    $("ul").text(word.reverse() + " ");
+    reversedWords.forEach(function(item){
+      str = str + item + " ";
+    });
+
+    $("ul").text(str);
 
   });
 
